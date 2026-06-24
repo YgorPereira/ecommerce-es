6cc5215f2eb00dc4215d1ac2efe74b286d8921b4
YgorPereira <ygorrp25@gmail.com>
2026-06-23 22:51:20 -0300

    fix: move entity code to right file

--- src/modules/users/entity.py
+++ src/modules/users/entity.py
@@ -1,1 +1,12 @@
-# Reponsável pelo domínio/regras de negócio
+from src.modules.users.enums.role import UserRole
+
+
+class User:
+    def __init__(self, name: str, cpf: str, email: str, role: UserRole):
+        self.name = name
+        self.cpf = cpf
+        self.email = email
+        self.role = role
+
+    def is_admin(self):
+        return self.role == UserRole.ADMIN
