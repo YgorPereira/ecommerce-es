a60ff6b594449eb6206583de7528699fb9a26a08
YgorPereira <ygorrp25@gmail.com>
2026-06-23 21:04:22 -0300

    style: format code with black formatter

--- src/database/session.py
+++ src/database/session.py
@@ -2,17 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from src.core.settings import settings
 
-
 engine = create_engine(
     settings.database_url,
     echo=True,
 )
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False
-)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 
 def get_db():
     db: Session = SessionLocal()
