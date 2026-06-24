585799c75e9bcde6eb23838077240630aafee327
YgorPereira <ygorrp25@gmail.com>
2026-06-23 21:44:18 -0300

    style: format test with black formatter

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,12 +1,15 @@
 import pytest
 
+
 @pytest.mark.unit
 def test_unit_ok():
     assert 1 + 1 == 2
 
+
 @pytest.mark.integration
 def test_integration_ok():
     assert "banco" == "banco"
+
 
 @pytest.mark.smoke
 def test_smoke_ok():
