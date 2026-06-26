d13ed2792fe35e80e4603d0dacebcbcdb03ae6e2
YgorPereira <ygorrp25@gmail.com>
2026-06-25 22:46:36 -0300

    feat: add initial fastapi + uvicorn server app

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from h11 import Request
 from src.modules.users.router import user_router
