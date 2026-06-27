0939f0037631ae10492057cbf724df814374878d
YgorPereira <ygorrp25@gmail.com>
2026-06-27 19:59:12 -0300

    refactor:  user mapper for repository

--- src/modules/users/entity.py
+++ src/modules/users/entity.py
@@ -1,12 +1,17 @@
+
 from src.modules.users.enums.role import UserRole
 
 
 class User:
-    def __init__(self, name: str, cpf: str, email: str, role: UserRole):
+    def __init__(self, id: uuid.UUID, name: str, cpf: str, email: str, role: UserRole, password: str):
+        self.id = id
         self.name = name
         self.cpf = cpf
         self.email = email
         self.role = role
+        self.password = password
+
 
     def is_admin(self):
         return self.role == UserRole.ADMIN
+
