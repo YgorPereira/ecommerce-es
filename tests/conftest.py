0284cb0856e739678d8632ca29800740b997e791
YgorPereira <ygorrp25@gmail.com>
2026-06-30 22:04:39 -0300

    ci: add env on

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,6 +25,7 @@ def postgres_temp_db():
         dbname=dbname,
         version=16,
         password=password,
+        drop_on_error=True,
     ):
         yield f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{dbname}"
 
