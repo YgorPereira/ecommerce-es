a6f7a945d667934d3763c8ec5158756740258c2c
Luana Souza <128157430+luanaapms@users.noreply.github.com>
2026-07-02 10:10:26 -0300

    feat: add products and categories migrations

--- src/modules/categories/models.py
+++ src/modules/categories/models.py
@@ -8,5 +8,5 @@ from src.database.idmixin import IdMixin
 class CategoryModel(Base, IdMixin):
     __tablename__ = "categories"
 
-    name: Mapped[str] = mapped_column(String(100), unique=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     description: Mapped[str] = mapped_column(String(255))
